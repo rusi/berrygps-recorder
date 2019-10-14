068a9850e6f0bfe130892e6cefe77c290c3098dd
Ruslan Hristov <rusi@nutonomy.com>
2019-10-13 23:13:09 -0400

    working state transitions

--- led.py
+++ led.py
@@ -2,9 +2,12 @@ import RPi.GPIO as GPIO
 import time
 
 BCM_led = -1
+terminate = False
 
 def bing(n, d = 0.2):
     for _ in xrange(n):
+        if terminate:
+            return
         GPIO.output(BCM_led, GPIO.HIGH)
         time.sleep(d)
         GPIO.output(BCM_led, GPIO.LOW)
@@ -12,16 +15,22 @@ def bing(n, d = 0.2):
     time.sleep(1.0)
 
 def encode(digit):
-    bing((digit // 100)%10 + 1, 0.8)
-    bing((digit // 10)%10 + 1, 0.4) 
-    bing((digit % 10) + 1, 0.2)
-    time.sleep(1.0)
+    if not terminate:
+        bing((digit // 100)%10 + 1, 0.8)
+    if not terminate:
+        bing((digit // 10)%10 + 1, 0.4) 
+    if not terminate:
+        bing((digit % 10) + 1, 0.2)
+    if not terminate:
+        time.sleep(1.0)
 
 def encodeIP(ipaddr):
     bing(3, 0.05)
     time.sleep(1.0)
     octets = ipaddr.split('.')
     for i, octet in enumerate(octets):
+        if terminate:
+            return
         time.sleep(1.0)
         #bing(i+1, 0.1)
         encode(int(octet))
--- recorder.py
+++ recorder.py
@@ -21,6 +21,7 @@ recording = False
 
 def displayIPaddress():
     global booting
+    print("booting...")
     while booting:
         ipaddr = commands.getoutput('hostname -I')
         led.bing(3, 0.05)
@@ -29,28 +30,43 @@ def displayIPaddress():
             print(ipaddr)
             led.encodeIP(ipaddr)
     GPIO.output(BCM_led, GPIO.LOW)
+    print("booting...DONE")
 
-def onBtnDown(channel):
+def onBtnPushed(channel):
     global booting
     global recording
+    print("pushed")
     booting = False
     recording = True
-    while recording:
+    led.terminate = True
+
+def record():
+    global recording
+    print("recording...")
+    while GPIO.input(BCM_btn):
         GPIO.output(BCM_led, GPIO.HIGH)
         time.sleep(0.1)
         GPIO.output(BCM_led, GPIO.LOW)
         time.sleep(1.0)
-
-def onBtnUp(channel):
-    global recording
     recording = False
+    print("recording...done")
 
-GPIO.add_event_detect(BCM_btn, GPIO.RISING, callback=onBtnDown)
-GPIO.add_event_detect(BCM_btn, GPIO.RISING, callback=onBtnUp)
+def idle():
+    GPIO.output(BCM_led, GPIO.HIGH)
+    time.sleep(0.2)
+    GPIO.output(BCM_led, GPIO.LOW)
+    time.sleep(4.0)
 
-displayIPaddress()
+GPIO.add_event_detect(BCM_btn, GPIO.RISING, callback=onBtnPushed)
 
-while True:
-    time.sleep(10)
+try:
+    displayIPaddress()
+    while True:
+        if recording:
+            record()
+        else:
+            idle()
 
-GPIO.cleanup()
+finally:
+    GPIO.cleanup()
+
